a069d001d852010dc2799ec67fc204db127e8d28
Seamus Malley <seamusmalley@Seamuss-MacBook-Pro.local>
2017-05-01 01:34:29 -0400

    added some variables

--- src/Body.java
+++ src/Body.java
@@ -1,1 +1,13 @@
 // Physical bodies for the simulation
+
+public class Body {
+        // Coordinates
+        double x, y, z;
+
+        // Physical dimensions
+        double mass, radius;
+        
+        // Orbit Information
+        double period;
+        double e;
+}
