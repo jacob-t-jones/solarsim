6fefd28eb73b4862e90cb7f7c0d043ffc25cebff
Seamus Malley <seamusmalley@Seamuss-MacBook-Pro.local>
2017-05-04 10:16:18 -0400

    added constants to Body class

--- src/Body.java
+++ src/Body.java
@@ -1,5 +1,7 @@
 // Physical bodies for the simulation
 
+import java.util.*;
+
 public class Body {
         // Coordinates
         double x, y, z;
@@ -8,6 +10,12 @@ public class Body {
         double mass, radius;
         
         // Orbit Information
-        double period;
-        double e;
+        double period, e, parallaxAngle;
+
+        // Universal Constants
+        final static double INVERSE_G = 14983338500;
+        final static double G         = 1 / INVERSE_G;
+        final static double PI        = Math.PI;
+
+
 }
